6deb21b9707ffc325d63122b2f56f43eae1ca527
Himanshu <mauryah380@gmail.com>
2023-05-31 02:00:54 +0530

    implemented deposit amount functionality

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1 -> Deposit some money
+// 2 -> Determine no. of linr to bet on
+// 3 -> collect a bet amount
+// 4 -> spin the slot machine
+// 5 -> check if the user won
+// 6 -> give the user their winnings
+// 7 -> play again
+
+
+
+const prompt = require('prompt-sync')();
+
+
+const deposit = () => {
+
+    while(true){
+        const depositAmount = prompt("Enter a deposit amount : ");
+
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try again.")
+        }
+        else{
+            return numberDepositAmount;
+        }
+    }
+}
+
+const depositAmount = deposit();
+console.log(depositAmount);
